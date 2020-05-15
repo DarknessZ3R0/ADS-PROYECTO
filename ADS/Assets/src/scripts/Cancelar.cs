1c9b20b12b23e52308fbcbbe9cdeab318ca24080
Efren Mario <mgonzalez97@uabc.edu.mx>
2020-05-14 23:53:33 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ ADS/Assets/src/scripts/Cancelar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Cancelar : MonoBehaviour
+{
+    public Button MiBoton;
+    public int numero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = MiBoton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TaskOnClick(){
+        
+        SceneManager.LoadScene("Home_Scene");
+        
+    }
+}
--- ADS/Assets/src/scripts/SceneLogin.cs
+++ ADS/Assets/src/scripts/SceneLogin.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor;
+using System.IO;
 
 public class SceneLogin : MonoBehaviour
 {
@@ -12,7 +14,8 @@ public class SceneLogin : MonoBehaviour
     public InputField User_password;
     public InputField User_id;
     public InputField User_birthday;
-    
+    //public GameObject mensaje;
+  
     void Start()
     {
         Button btn = MiBoton.GetComponent<Button>();
@@ -22,9 +25,39 @@ public class SceneLogin : MonoBehaviour
         User_birthday = User_birthday.GetComponent<InputField>();
         btn.onClick.AddListener(TaskOnClick);
     }
+    //[MenuItem("Tools/Write file")]
+     void WriteString()
+     {
+         string path = "Assets/src/scripts/Usuarios.txt";
+         string mail = User_mail.text.ToString();
+         StreamWriter writer = new StreamWriter(path, true);
+         writer.WriteLine(mail);
+         writer.Close();
+     }
+ 
+ //[MenuItem("Tools/Read file")]
+ static void ReadString(){
+ 
+        string path = "Assets/src/scripts/test.txt";
+         //Read the text from directly from the test.txt file
+         StreamReader reader = new StreamReader(path); 
+         Debug.Log(reader.ReadToEnd());
+         reader.Close();
+         
+     }
     void TaskOnClick(){
-        
-        SceneManager.LoadScene("Home_Scene");
+        if((User_mail.text == "") && (User_password.text == "") && (User_id.text == "") && (User_birthday.text == "") ){
+            //mensaje.SetActive(true);
+            Debug.Log("No Entro");
+        }else{
+            WriteString();
+            //implementar el read de algun archivo y una busqueda de 
+            //usuarios para validar el login, No es muy necesario 
+            //pero quedaria mamalon xD
+            // SceneManager.LoadScene("Home_Scene");
+            Debug.Log("Entro");
+        }
+       
         
     }
   
