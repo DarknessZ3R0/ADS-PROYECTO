a17473290834c2c069c0cdc175b6673b66425cd2
Efren Mario <mgonzalez97@uabc.edu.mx>
2020-05-15 00:42:12 -0700

    Login
    validado

--- ADS/Assets/src/scripts/Login.cs
+++ ADS/Assets/src/scripts/Login.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using UnityEditor;
+using System.IO;
 public class Login : MonoBehaviour
 {
     public InputField User_mail;
@@ -24,6 +25,36 @@ public class Login : MonoBehaviour
     {
         
     }
+     void ReadString(){
+         bool mail =false;
+         bool pssw = false;
+
+         string cmp;
+        string path = "Assets/src/scripts/Usuarios.txt";
+         //Read the text from directly from the test.txt file
+         StreamReader reader = new StreamReader(path); 
+         
+         do{
+             cmp = reader.ReadLine();
+             if(cmp == User_mail.text){
+                 mail =true;
+                cmp = reader.ReadLine();
+                if(cmp == password.text){
+                    pssw = true;
+                    break;
+                }   
+             }else{
+                 Debug.Log("No encontrado");
+             }
+         }while(cmp != null);
+         if(mail == true && pssw == true){
+             SceneManager.LoadScene("ApplicationScene");
+         }else{
+             Debug.Log("Usuario No encontrado");
+         }
+         reader.Close();
+         
+     }
     void TaskOnClick(){
         if((User_mail.text == "") && (password.text == "")){
             mensaje.SetActive(true);
@@ -31,7 +62,8 @@ public class Login : MonoBehaviour
             //implementar el read de algun archivo y una busqueda de 
             //usuarios para validar el login, No es muy necesario 
             //pero quedaria mamalon xD
-             SceneManager.LoadScene("ApplicationScene");
+            //SceneManager.LoadScene("ApplicationScene");
+            ReadString();
         }
        
         
--- ADS/Assets/src/scripts/SceneLogin.cs
+++ ADS/Assets/src/scripts/SceneLogin.cs
@@ -29,33 +29,25 @@ public class SceneLogin : MonoBehaviour
      void WriteString()
      {
          string path = "Assets/src/scripts/Usuarios.txt";
-         string mail = User_mail.text.ToString();
+         //string mail = User_mail.text.ToString();
          StreamWriter writer = new StreamWriter(path, true);
-         writer.WriteLine(mail);
+         writer.WriteLine(User_mail.text);
+         writer.WriteLine(User_password.text);
+         writer.WriteLine(User_id.text);
+         writer.WriteLine(User_birthday.text);
          writer.Close();
      }
  
  //[MenuItem("Tools/Read file")]
- static void ReadString(){
- 
-        string path = "Assets/src/scripts/test.txt";
-         //Read the text from directly from the test.txt file
-         StreamReader reader = new StreamReader(path); 
-         Debug.Log(reader.ReadToEnd());
-         reader.Close();
-         
-     }
+   
     void TaskOnClick(){
         if((User_mail.text == "") && (User_password.text == "") && (User_id.text == "") && (User_birthday.text == "") ){
             //mensaje.SetActive(true);
             Debug.Log("No Entro");
         }else{
-            WriteString();
-            //implementar el read de algun archivo y una busqueda de 
-            //usuarios para validar el login, No es muy necesario 
-            //pero quedaria mamalon xD
-            // SceneManager.LoadScene("Home_Scene");
-            Debug.Log("Entro");
+            WriteString();//check
+            SceneManager.LoadScene("Home_Scene");
+            //Debug.Log("Entro");
         }
        
         
