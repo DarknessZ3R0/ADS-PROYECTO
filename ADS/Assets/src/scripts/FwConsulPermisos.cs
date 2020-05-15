2d1cba7feb738e78469f43d71e47a5e114ecd826
Coki <cokitorresa@hotmail.com>
2020-05-14 23:09:07 -0700

    Avance ADS
    Coki Permisos

---
+++ ADS/Assets/src/scripts/FwConsulPermisos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FwConsulPermisos : MonoBehaviour
+{
+    public Button BkPerm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BkPerm = BkPerm.GetComponent<Button>();
+        BkPerm.onClick.AddListener(TaskOnClick);
+    
+    }
+
+    void TaskOnClick(){
+        SceneManager.LoadScene("ConsultarPermisos");
+    }
+   
+}
